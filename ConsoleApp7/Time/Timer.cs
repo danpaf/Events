e8a78e3a3788e55debe6f49c71260b4039676609
dp21 <pavlovdp21@st.ithub.ru>
2023-10-19 13:13:48 +0300

    init

---
+++ ConsoleApp7/Time/Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Timers;
+
+namespace ConsoleApp7;
+
+class Timer
+{
+    public event EventHandler Tick;
+
+    public Timer()
+    {
+        var timer = new System.Timers.Timer(1000);
+        timer.Elapsed += OnTick;
+        timer.AutoReset = true;
+        timer.Enabled = true;
+    }
+
+    private void OnTick(object sender, ElapsedEventArgs e)
+    {
+        Tick?.Invoke(this, EventArgs.Empty);
+    }
+}
