9e403d87e8e59e25dc83b8d48648f04d7a95c647
dp21 <pavlovdp21@st.ithub.ru>
2023-10-19 13:13:39 +0300

    init

---
+++ ConsoleApp7/Time/Counter.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp7;
+
+
+class Counter
+{
+    private int count = 0;
+
+    public Counter(Timer timer)
+    {
+        timer.Tick += OnTick;
+    }
+
+    private void OnTick(object sender, EventArgs e)
+    {
+        count++;
+        Console.WriteLine($"Counter value: {count}");
+    }
+}
