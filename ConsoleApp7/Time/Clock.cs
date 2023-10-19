5273304f6d069b798f69a73913365709baadcfba
dp21 <pavlovdp21@st.ithub.ru>
2023-10-19 13:13:30 +0300

    init

---
+++ ConsoleApp7/Time/Clock.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp7;
+
+class Clock
+{
+    public Clock(Timer timer)
+    {
+        timer.Tick += OnTick;
+    }
+
+    private void OnTick(object sender, EventArgs e)
+    {
+        Console.WriteLine($"Current time: {DateTime.Now.ToLongTimeString()}");
+    }
+}
