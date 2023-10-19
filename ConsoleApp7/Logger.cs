d07c4ca41a185a6d47406de60de0a52ca377ca68
dp21 <pavlovdp21@st.ithub.ru>
2023-10-19 13:13:02 +0300

    Initial commit

---
+++ ConsoleApp7/Logger.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleApp7.Bank;
+
+namespace ConsoleApp7;
+
+public class Logger
+{
+    public string LogFile { get; }
+    public Account Account { get; }
+
+    public Logger(string logFile, Account account)
+    {
+        LogFile = logFile;
+        Account = account;
+        Account.BalanceChanged += LogBalanceChange;
+    }
+
+    public void LogBalanceChange(decimal balance)
+    {
+        using (System.IO.StreamWriter file =
+               new System.IO.StreamWriter(LogFile, true))
+        {
+            file.WriteLine($"The balance is {balance}");
+        }
+    }
+}
